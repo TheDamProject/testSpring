e17ea7d45682c81f72607b8b25e47da8e5a6133a
Juan M <juanma887@gmail.com>
2020-11-27 19:44:43 +0100

    prueba

--- src/main/java/com/jotamarti/demo/dao/PersonDao.java
+++ src/main/java/com/jotamarti/demo/dao/PersonDao.java
@@ -2,6 +2,8 @@ package com.jotamarti.demo.dao;
 
 import com.jotamarti.demo.model.Person;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -12,6 +14,14 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
 
 
 
