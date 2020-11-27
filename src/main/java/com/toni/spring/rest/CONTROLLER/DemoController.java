538883658e29892b568351812f241d94dcf06733
Antonio Ferrer <buceocv@gmail.com>
2020-11-27 21:33:46 +0100

    Mi primera version

--- src/main/java/com/toni/spring/rest/CONTROLLER/DemoController.java
+++ src/main/java/com/toni/spring/rest/CONTROLLER/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -18,14 +19,34 @@ public class DemoController {
     @GetMapping("/inicio")
     public String inicio(@RequestParam(name="name" , required = false, defaultValue = "Mundo") String name, Model model){
 
-        Person person1 = new Person();
-        person1.setId(1);
-        person1.setName("Antonio");
 
-        repoPerson.save(person1);
 
         model.addAttribute("name" , name);
         return "index";
 
     }
+    @GetMapping("/inserta")
+    public String inicio(@RequestParam(name="name" , required = false, defaultValue = "NONAME") String name,
+                         @RequestParam(name="id" , required = false, defaultValue = "999") Long id,Model model){
+
+        Person person1 = new Person();
+        person1.setId(id);
+        person1.setName(name);
+        repoPerson.save(person1);
+        repoPerson.save(person1);
+        return "index";
+
+    }
+
+    @GetMapping("/lista")
+    public String personas(Model model){
+
+        model.addAttribute("personas", repoPerson.findAll() );
+        model.addAttribute("name" , repoPerson.getOne((long) 1).getName());
+        return "index";
+    }
+
+
+
+
 }
