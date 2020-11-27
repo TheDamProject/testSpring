ccd44474e634c1f15fd21d499739b2afa6324691
Antonio Ferrer <buceocv@gmail.com>
2020-11-27 18:43:14 +0100

    Cambio el pom

--- src/main/java/com/jotamarti/demo/dao/PersonDao.java
+++ src/main/java/com/jotamarti/demo/dao/PersonDao.java
@@ -2,6 +2,7 @@ package com.jotamarti.demo.dao;
 
 import com.jotamarti.demo.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -12,7 +13,7 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
-
+    List<Person> selectAllPeople();
 
 
 }
